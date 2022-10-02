31fe101e4ef65a80abeeb72621f6e8c899e2577d
achal <achalmeshram966@gmail.com>
2022-10-02 22:03:10 +0530

    greatest number

---
+++ 2oct.py
@@ -0,0 +1,17 @@
+weekday = int(input("Enter the number(1-7):"))
+if weekday == 1:
+    print("monday")
+elif weekday == 2:
+    print("tuesday")
+elif weekday == 3:
+    print("wednsday") 
+elif weekday == 4:
+    print("thursday") 
+elif weekday == 5:
+    print("friday") 
+elif weekday == 6:
+    print("saterday")     
+elif weekday == 7:
+    print("sunday")
+else:
+    print("Enter the any weekday")    
